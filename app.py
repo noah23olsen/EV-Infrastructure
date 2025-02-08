e01a629f2b4528f694ba7cba32020f2b4c3c702e
Noah Olsen <noah23olsen@gmail.com>
2025-02-08 18:28:16 -0500

    cleanup v2

--- app.py
+++
@@ -1,7 +0,0 @@
-import kagglehub
-import requests
-
-# Download latest version
-path = kagglehub.dataset_download("prasertk/electric-vehicle-charging-stations-in-usa")
-
-print("Path to dataset files:", path)
